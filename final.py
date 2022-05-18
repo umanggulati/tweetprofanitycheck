080bfb0d9eef9ace0f65470fba3466a2264ae208
Umang <umang.gulati19@gmail.com>
2022-05-19 03:10:50 +0530

    The purpose of this comit is to duplicate original tweets, removing null and duplicate values and resetting index

--- final.py
+++ final.py
@@ -17,6 +17,10 @@ def load_data():
     data.columns = ["datetime", "tweets", "user", "location"]
     return data
 
-
+tweet_df = load_data()
+tweet_df["originaltweets"] = tweet_df["tweets"]  # duplicating original tweets
+tweet_df = tweet_df.dropna()  # removing rows containing null values
+tweet_df = tweet_df.drop_duplicates()  # removing duplicates
+tweet_df.reset_index(inplace=True)  # resetting index after removing duplicates
 
 
