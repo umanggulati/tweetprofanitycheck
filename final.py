8c2a9e0e01f733f5f92559ed72d9a1fe0f87098c
Umang <umang.gulati19@gmail.com>
2022-05-19 03:22:03 +0530

    tweet preprocessing
    This change preprocess the tweets by lowercasing letters, removing url,
    removing mentions,and removing hashtags,puntuations and patterns and
    finally tokenizing it

--- final.py
+++ final.py
@@ -23,4 +23,21 @@ tweet_df = tweet_df.dropna()  # removing rows containing null values
 tweet_df = tweet_df.drop_duplicates()  # removing duplicates
 tweet_df.reset_index(inplace=True)  # resetting index after removing duplicates
 
-
+# data cleaning
+# lowercasing all the letters
+tweet_df['tweets'] = tweet_df['tweets'].str.lower()
+# removing url
+tweet_df['tweets'] = tweet_df['tweets'].str.replace(
+    r'https?://[^ ]+', '', regex=True)
+tweet_df['tweets'] = tweet_df['tweets'].str.replace(
+    r'@[^ ]+', '', regex=True)  # removing mentions
+# removing a pattern in the file
+E = r'\\[a-zA-Z0-9-]+\\[a-zA-Z0-9]+\\[a-zA-Z0-9]+'
+tweet_df['tweets'] = tweet_df['tweets'].str.replace(E, '', regex=True)
+tweet_df['tweets'] = tweet_df['tweets'].str.replace(
+    r'b\'', '', regex=True)  # removing a pattern
+tweet_df['tweets'] = tweet_df['tweets'].str.replace(
+    r'#', '', regex=True)  # hashtag removal
+# removing all the punctuations
+tweet_df['tweets'] = tweet_df['tweets'].str.replace(r'[^\w\s]', '', regex=True)
+tweet_df['tweets'] = tweet_df['tweets'].apply(word_tokenize)  # tokenzing
